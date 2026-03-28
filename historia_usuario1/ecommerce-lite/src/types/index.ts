cfdfb49b8ed5539d028a9eb2b94c724a96506a30
JuanDavidsh23 <santamariajuan34@gmail.com>
2026-03-28 08:48:31 -0500

    feat: initial commit with historia_usuario1

---
+++ historia_usuario1/ecommerce-lite/src/types/index.ts
@@ -0,0 +1,10 @@
+import { Product } from "../interfaces/product";
+import { User } from "../interfaces/user";
+
+export interface ProductCardProps {
+  product: Product;
+}
+
+export interface UserCardProps {
+  user: User;
+}
